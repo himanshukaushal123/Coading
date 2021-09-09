559f6738dee76cea1bd220f8fef2b31c98fbf7d2
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2021-09-09 15:31:00 +0530

    https://codeforces.com/problemset/problem/1537/B

---
+++ codeFoece/bad_boy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int t, n, m, x, y;
+    cin>>t;
+    while (t--)
+    {
+        cin >> n >> m ;
+            cin >> x >> y;
+    
+        {
+            cout << "1"
+                 << " "
+                 << "1" <<" "<< n << " " << m<<endl;
+        }
+    }
+}
