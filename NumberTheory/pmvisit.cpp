83f8e8b0f8fc3cce788d696962ae42b6d37cda7c
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2021-10-23 11:50:02 +0530

    PM visit
    PMO gives two random numbers a & b to the Prime Minister. PM Modi wants to visit all countries between a and b (inclusive) , However due to shortage of time he can't visit each and every country , So PM Modi decides to visit only those countries,for which country number has two divisors. So your task is to find number of countries Mr.Modi will visit.

---
+++ NumberTheory/pmvisit.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+// We check until "root of N" if that number of divisior having in this range
+// beacuse divisiors are occuring in pair eg 36-2,3,4,6,9,12,18 if you have 2 then you
+// also have 18 and so on. o(N.sqrt(N))
+bool prime(int n)
+{
+    if (n == 1)
+    {
+        return false;
+    }
+    if (n == 2)
+    {
+        return true;
+    }
+    for (int i = 2; i * i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+// Create a array that contains Prime number
+// mark multiple of 2 as not prime then move to
+// the the next number and see weather given number is mark or
+// not if not marked then marked the multiple of that number and so on
+// time complexity (N/2+N/3+N/5+N/7+...)=O(NLogLogN)=Approxmitly in o(1) i.e liner
+
+void primeSeive(int *a)
+{
+    // mark all odd numbers as prime
+    for (int i = 3; i < 100000; i += 2)
+    {
+        a[i] = 1;
+    }
+    // seive
+    for (int i = 3; i <= 100000; i += 2)
+    {
+        if (a[i] == 1)
+            // mark all the multiple of i as not prime
+            for (long long j = /*2 * i*/ i * i; j <= 100000; j += i)
+            {
+                a[j] = 0;
+            }
+    }
+    // special case
+    a[2] = 1;
+    a[1] = a[1] = 0;
+}
+
+int main()
+{
+    int a[100005] = {0};
+    primeSeive(a);
+
+    int csum[100005]={0};
+    //precomptute
+    for (int i = 1; i <= 100000; i++)
+    {
+        csum[i] = csum[i - 1] + a[i];
+    }
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int start, end;
+        cin >> start >> end;
+        cout << csum[end] - csum[start - 1] << endl;
+    }
+    return 0;
+}
