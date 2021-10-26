762509d48ee8d6c9101e88c57bef5625748b3e86
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2021-10-26 09:51:15 +0530

    Prime_Factorization_without_Seive

---
+++ NumberTheory/primefactorizationNoseive.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+void primeFactor(int n){
+    int cnt;
+    vector<pair<int,int>> factors;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            cnt=0;
+            while(n%i==0){
+                cnt++;
+                n=n/i;
+            }
+            factors.push_back(make_pair(i,cnt));
+        }
+    }
+    if(n!=1){
+       factors.push_back(make_pair(n,1)); 
+    }
+
+    for(auto elm:factors){
+        cout<<elm.first<<"^"<<elm.second<<endl;
+    }
+}
+int main(){
+    primeFactor(132);
+    return 0;
+}
