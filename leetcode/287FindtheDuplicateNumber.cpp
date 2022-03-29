7d20043923972dca1a9bb4961724f8e86a499662
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-03-29 12:09:17 +0530

    Create 287FindtheDuplicateNumber.cpp

---
+++ leetcode/287FindtheDuplicateNumber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+     vector<int> vec(100001,-1);
+        for(int i=0;i<nums.size();i++){
+            if(vec[nums[i]]==-1){
+                vec[nums[i]]=0;
+            }else if(vec[nums[i]]==0){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
