0ed735da7b84d760d60419bf7629262973798fb8
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-05 13:01:38 +0530

    Update 1019NextGreaterNodeInLinkedList.cpp

--- leetcode/1019NextGreaterNodeInLinkedList.cpp
+++ leetcode/1019NextGreaterNodeInLinkedList.cpp
@@ -12,3 +12,21 @@ public:
   return res;
 }
 };
+//second way
+vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> ans;
+        stack<pair<int,int>> s;
+        int i=0;
+        while(head){
+            while(!s.empty() && s.top().first<head->val){
+                int idx=s.top().second;
+                s.pop();
+                ans[idx]=head->val;
+            }
+            s.push({head->val,i++});
+            head=head->next;
+            ans.push_back(0);
+        }
+        return ans;
+    }
+};
