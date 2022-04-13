e606e6eed5bdeca833590a9e5dccbb403b9d2e5c
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-13 11:23:42 +0530

    Update Kthsmallestelement.cpp

--- LoveBabber/Kthsmallestelement.cpp
+++ LoveBabber/Kthsmallestelement.cpp
@@ -11,3 +11,16 @@ class Solution{
         return arr[k-1];
     }
 };
+//Second Method TC=O(logk)
+ int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+       priority_queue<int> pq;
+        for(int i=0;i<=r;i++){
+             pq.push(arr[i]);
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
