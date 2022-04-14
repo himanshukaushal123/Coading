f0a07c75b9f3528daddc2d48e51c55624aa3acb7
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-14 12:28:42 +0530

    Update 701. Insert into a Binary Search Tree.cpp

--- leetcode/701. Insert into a Binary Search Tree.cpp
+++ leetcode/701. Insert into a Binary Search Tree.cpp
@@ -1,3 +1,4 @@
+//1st method
 TreeNode* insertIntoBST(TreeNode* root, int val) {
        if(root==NULL){return new TreeNode(val);}
         TreeNode* cur=root;
@@ -23,3 +24,18 @@ TreeNode* insertIntoBST(TreeNode* root, int val) {
        }
         return root;
     }
+
+//2nd method
+TreeNode* insertIntoBST(TreeNode *node, int val) {
+		if (!node) {
+			TreeNode *newNode = new TreeNode(val);
+			return newNode;
+		}
+		if (val < node->val) {
+			node->left = insertIntoBST(node->left, val);
+		}
+		else {
+			node->right = insertIntoBST(node->right, val);
+		}
+		return node;
+	}
