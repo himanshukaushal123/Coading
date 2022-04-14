2b58c963b04331028e5118d7c533c8177586c836
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-14 12:26:06 +0530

    Create 701InsertintoaBinarySearchTree.cpp

---
+++ 701InsertintoaBinarySearchTree.cpp
@@ -0,0 +1,14 @@
+//1st method
+TreeNode* insertIntoBST(TreeNode *node, int val) {
+		if (!node) {
+			TreeNode *newNode = new TreeNode(val);
+			return newNode;
+		}
+		if (val < node->val) {
+			node->left = insertIntoBST(node->left, val);
+		}
+		else {
+			node->right = insertIntoBST(node->right, val);
+		}
+		return node;
+	}
