5d7ec9cad0bae04f5fa71842a579574da0378f37
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-15 13:03:35 +0530

    Update 102BinaryTreeLevelOrderTraversal.cpp

--- leetcode/102BinaryTreeLevelOrderTraversal.cpp
+++ leetcode/102BinaryTreeLevelOrderTraversal.cpp
@@ -41,3 +41,36 @@ public:
       return vec;
     }
 };
+
+//2nd method
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>vec;
+        vector<int>ans;
+        if(root==NULL) return vec;
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()){
+            TreeNode * temp=q.front();
+            if(temp==NULL){
+                vec.push_back(ans);
+                ans.clear();
+                q.pop();
+            if(!q.empty()){
+                q.push(NULL);
+            }
+          }
+          else{
+              q.pop();
+              ans.push_back(temp->val);
+              if(temp->left)
+              q.push(temp->left);
+              if(temp->right)
+              q.push(temp->right);
+            }
+        }
+        return vec;
+    }
+};
