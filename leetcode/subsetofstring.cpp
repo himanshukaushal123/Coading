3985ebf6fe0a64f7bcb3899a7cf29b2658b2b73a
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-19 13:11:03 +0530

    Update subsetofstring.cpp

--- leetcode/subsetofstring.cpp
+++ leetcode/subsetofstring.cpp
@@ -1,23 +1,17 @@
-#include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
-void solve(string &in,string &op){
+void solve(string in,string op){
     if(in.length()==0) {
         cout<<"1"<<op<<endl; 
         return;
     }
-        string op1=op;
-        string op2=op;
-        op1.push_back(in[0]);
-        // cout<<op1<<" ";
-        in.erase(in.begin());
-        // cout<<in<<" ";
-        solve(in,op1);
-        solve(in,op2);
-        return;
+    solve(in.substr(1),op+in[0]);
+    solve(in.substr(1),op);
+    return;
 } 
 int main(){
-    string in="ab";
+    string in="abc";
     string op="";
     solve(in,op);
     return 0;
