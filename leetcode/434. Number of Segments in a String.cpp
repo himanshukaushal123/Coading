d68a81f6d7f73f2a54706d2d1a71319bd058f58d
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-20 13:08:12 +0530

    Create 434. Number of Segments in a String.cpp

---
+++ leetcode/434. Number of Segments in a String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSegments(string s) {
+   stringstream str(s);
+ 
+    string word;
+    int count = 0;
+    while (str >> word)
+        count++;
+    return count;
+        
+    }
+};
