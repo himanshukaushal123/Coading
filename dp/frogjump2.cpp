ba031d4813298bbefb5cfacda1244de23d804dab
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-25 18:36:16 +0530

    Create frogjump2.cpp

---
+++ dp/frogjump2.cpp
@@ -0,0 +1,38 @@
+//https://atcoder.jp/contests/dp/tasks/dp_b
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solveUtil(int ind, vector<int>& height, vector<int>& dp, int k){
+    if(ind==0) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+    
+    int mmSteps = INT_MAX;
+        
+    for(int j=1;j<=k;j++){
+        if(ind-j>=0){
+      int jump = solveUtil(ind-j, height, dp, k)+ abs(height[ind]- height[ind-j]);
+            mmSteps= min(jump, mmSteps);
+        }
+    }
+    return dp[ind]= mmSteps;
+    
+}
+
+int solve(int n, vector<int>& height , int k){
+    vector<int> dp(n,-1);
+    return solveUtil(n-1, height, dp, k);
+}
+
+int main() {
+
+  int n,k;
+  cin>>n>>k;
+  vector<int> height;
+  for(int i=0;i<n;i++){
+    cin>>height[i];
+  }
+  vector<int> dp(n,-1);
+  cout<<solve(n,height,k);
+}
