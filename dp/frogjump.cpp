7448ee4d6cddf270c0799fbc9e58b5b64cd3a2c8
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-25 15:35:12 +0530

    Update frogjump.cpp

--- dp/frogjump.cpp
+++ dp/frogjump.cpp
@@ -18,3 +18,19 @@ int frogJump(int n, vector<int> &heights)
 	return f(n-1,heights,dp);
 }
 
+//2nd Method
+#include<bits/stdc++.h>
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+	vector<int>dp(n,0);
+	dp[0]=0;
+	for(int i=1;i<n;i++){
+		int f=dp[i-1]+abs(heights[i]-heights[i-1]);
+		int s=INT_MAX;
+		if(i>1)s=dp[i-2]+abs(heights[i]-heights[i-2]);
+		dp[i]=min(s,f);
+	}
+	return dp[n-1];
+}
+
