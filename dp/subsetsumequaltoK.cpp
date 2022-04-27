231a2abbac36f44543b5900db9a6b7def17bc696
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-27 14:02:03 +0530

    Update subsetsumequaltoK.cpp

--- dp/subsetsumequaltoK.cpp
+++ dp/subsetsumequaltoK.cpp
@@ -45,3 +45,22 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
   }
       return dp[n-1][k];
 }
+//space optimization
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
+	vector<bool>prev(k+1,0),curr(k+1,0);
+	prev[0]=curr[0]=true;
+	prev[arr[0]]=true;
+	 for(int i=1;i<n;i++){
+      for(int j=1;j<=k;j++){
+            bool notTake=prev[j];
+            bool take=false;
+             if(arr[i]<=j){
+                 take=prev[j-arr[i]];
+              }
+              curr[j]=take|notTake;
+      }
+		 prev=curr;
+  }
+      return prev[k];
+}
