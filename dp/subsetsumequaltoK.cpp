5e17c1a0f597a633ecf9c20c54836a7605408a95
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-27 13:48:30 +0530

    Update subsetsumequaltoK.cpp

--- dp/subsetsumequaltoK.cpp
+++ dp/subsetsumequaltoK.cpp
@@ -27,3 +27,21 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
 	vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
 	return subsetSumTo(n,k,arr,dp);
 }
+//tabulation
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
+	vector<vector<bool>>dp(n,vector<bool>(k+1,0));
+	for(int i=0;i<n;i++) dp[i][0]=true;
+	dp[0][arr[0]]=true;
+	 for(int i=1;i<n;i++){
+      for(int j=1;j<=k;j++){
+            bool notTake=dp[i-1][j];
+            bool take=false;
+             if(arr[i]<=j){
+                 take=dp[i-1][j-arr[i]];
+              }
+              dp[i][j]=take|notTake;
+      }
+  }
+      return dp[n-1][k];
+}
