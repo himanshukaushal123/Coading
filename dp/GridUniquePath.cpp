80e31aa2787108c371ad6456ceb3a792ea9ade57
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-27 16:32:18 +0530

    Update GridUniquePath.cpp

--- dp/GridUniquePath.cpp
+++ dp/GridUniquePath.cpp
@@ -10,7 +10,7 @@ int uniquePaths(int m, int n) {
 	// Write your code here.
 	return solve(m-1,n-1);
 }
-//memorization TC(N*2)
+//memorization TC(N*M); SC(O((N-1)+(M-1))+O(M*N)<-DP(TOP-DOWN APPROCH)
 int solve(int m,int n,vector<vector<int>>&dp){
 	if(m==0&&n==0) return 1;
 	if(m<0 || n<0)return 0;
@@ -24,4 +24,20 @@ int uniquePaths(int m, int n) {
 	vector<vector<int>>dp(m,vector<int>(n+1,-1));
 	return solve(m-1,n-1,dp);
 }
-//tabulation
+//tabulation(BOTTOM UP APPROCH) TC O(N*M) SC(N*M)
+int uniquePaths(int m, int n) {
+	// Write your code here.
+	int dp[m][n];
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			if(i==0&&j==0)dp[0][0]=1;
+			else{
+				int left=0,up=0;
+				if(i>0)up=dp[i-1][j];
+	             if(j>0)left=dp[i][j-1];
+	             dp[i][j]=up+left;
+			}
+		}
+	}
+	return dp[m-1][n-1];
+}
