f80b366d20e67042520a01c13a8fed85c455f8a5
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-28 12:14:08 +0530

    Update Triangle.cpp

--- dp/Triangle.cpp
+++ dp/Triangle.cpp
@@ -23,3 +23,20 @@ int minimumPathSum(vector<vector<int>>& triangle, int n){
 	vector<vector<int>>dp(n,vector<int>(m+1,-1));
 	return solve(triangle,n,0,0,dp);
 }
+//tabulation
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+	// Write your code here.
+	int m=triangle[n-1].size();
+	vector<vector<int>>dp(n,vector<int>(m+1,-1));
+	for(int i=n-1;i>=0;i--){
+		for(int j=i;j>=0;j--){
+			if(i==n-1)dp[i][j]=triangle[i][j];
+			else{
+			    int d=triangle[i][j]+dp[i+1][j];
+	            int dg=triangle[i][j]+dp[i+1][j+1];
+	            dp[i][j]=min(d,dg);
+			}
+		}
+	}
+	return dp[0][0];
+}
