2663aca18de6bf101ca33e2bba05266a85ddf910
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-28 17:55:29 +0530

    Update Maximum Path Sum in the matrix.cpp

--- dp/Maximum Path Sum in the matrix.cpp
+++ dp/Maximum Path Sum in the matrix.cpp
@@ -19,3 +19,25 @@ int getMaxPathSum(vector<vector<int>> &matrix)
 	}
 	return maxi;
 }
+//memrization
+int solve(int i,int j,vector<vector<int>> &matrix,vector<vector<int>>&dp){
+	if(j<0||j>=matrix[0].size())return -1e9;
+	if(i==0)return matrix[0][j];
+	if(dp[i][j]!=-1)return dp[i][j];
+	int left=matrix[i][j]+solve(i-1,j-1,matrix,dp);
+	int right=matrix[i][j]+solve(i-1,j+1,matrix,dp);
+	int up=matrix[i][j]+solve(i-1,j,matrix,dp);
+	return dp[i][j]=max(left,max(right,up));
+}
+int getMaxPathSum(vector<vector<int>> &matrix)
+{
+    //  Write your code here.
+	int m=matrix.size();
+	int n=matrix[0].size();
+	int maxi=-1e8;
+	vector<vector<int>>dp(m,vector<int>(n+1,-1));
+	for(int j=0;j<n;j++){
+		maxi=max(maxi,solve(m-1,j,matrix,dp));
+	}
+	return maxi;
+}
