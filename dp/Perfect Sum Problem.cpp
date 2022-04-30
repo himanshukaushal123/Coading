ff53115fb21e9b7de8ded9170cba6fbf226ee460
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-04-30 11:50:45 +0530

    Create Perfect Sum Problem.cpp

---
+++ dp/Perfect Sum Problem.cpp
@@ -0,0 +1,26 @@
+int perfectSum(int arr[], int n, int k){
+        long long m = 1e9 + 7;
+        long long int dp[n+1][k+1]={};
+        for(int i=0;i<n+1;i++)
+        {
+        for(int j=0;j<k+1;j++)
+        {
+          if(i==0)    
+              dp[i][j]=0;
+          if(j==0)
+              dp[i][j]=1;
+        }     
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<k+1;j++){
+                if(arr[i-1]<=j){
+                  dp[i][j]= (dp[i-1][j-arr[i-1]]%m + dp[i-1][j]%m)%m;
+                }else{
+                  dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][k];
+    }
+  
+};
