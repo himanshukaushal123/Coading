9b70eb8f5ce4d37d052b99235b97887a65d0e992
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-05-02 11:31:04 +0530

    Update 905. Sort Array By Parity.cpp

--- leetcode/905. Sort Array By Parity.cpp
+++ leetcode/905. Sort Array By Parity.cpp
@@ -21,3 +21,17 @@ public:
         
     }
 };
+//2nd method
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+         int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        return nums;
+    }
+};
