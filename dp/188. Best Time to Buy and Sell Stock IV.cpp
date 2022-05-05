8000b0676c5f12724575c5a57fe90b2b277f8d73
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-05-05 11:52:52 +0530

    Create 188. Best Time to Buy and Sell Stock IV.cpp

---
+++ dp/188. Best Time to Buy and Sell Stock IV.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int ind,int buy,int cap,int profit,vector<int>&prices,vector<vector<vector<int>>>&dp){
+        if(cap==0)return 0;
+        if(ind==prices.size())return 0;
+        if(dp[ind][buy][cap]!=-1){
+            return dp[ind][buy][cap];
+        }
+        if(buy){
+            profit= max(-prices[ind]+solve(ind+1,0,cap,profit,prices,dp),solve(ind+1,1,cap,profit,prices,dp));
+        }else{
+            profit=max(prices[ind]+solve(ind+1,1,cap-1,profit,prices,dp),solve(ind+1,0,cap,profit,prices,dp));
+        }
+        return dp[ind][buy][cap]=profit;
+    }
+    int maxProfit(int cap,vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(cap+1,-1)));
+        return solve(0,1,cap,INT_MIN,prices,dp);
+    }
+};
