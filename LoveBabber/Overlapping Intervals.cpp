ff19e55dc1969cbe60fe1cff608b51e5849afc70
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-05-10 14:35:50 +0530

    Create Overlapping Intervals.cpp

---
+++ LoveBabber/Overlapping Intervals.cpp
@@ -0,0 +1,18 @@
+vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
+      vector<vector<int>>mergedIntervals;
+        if(intervals.size()==0){
+            return mergedIntervals;
+        }
+        sort(intervals.begin(),intervals.end());
+        vector<int> tempInterval=intervals[0];
+        for(auto it:intervals){
+            if(it[0]<=tempInterval[1]){
+                tempInterval[1]=max(it[1],tempInterval[1]);
+            }else{
+                mergedIntervals.push_back(tempInterval);
+                tempInterval=it;
+            }
+        }
+        mergedIntervals.push_back(tempInterval);
+        return mergedIntervals;
+    }
