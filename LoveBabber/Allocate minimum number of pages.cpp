1722b8c87262ed48edc49f6760f26756ccc4e923
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-05-17 13:10:22 +0530

    Create Allocate minimum number of pages.cpp

---
+++ LoveBabber/Allocate minimum number of pages.cpp
@@ -0,0 +1,25 @@
+ bool isPossible(int A[],int m,int M,int n){
+        int c=1,sum=0;
+        for(int i=0;i<n;i++){
+            if(sum+A[i]<=m)
+                sum+=A[i];
+            else{
+                c++;
+                sum=A[i];
+            }
+        }
+        return c<=M;
+    }
+    int findPages(int A[], int N, int M) 
+    {
+        int l=*max_element(A,A+N);
+        int r=accumulate(A,A+N,0);
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(isPossible(A,m,M,N))
+                r=m-1;
+            else
+                l=m+1;
+        }
+        return r+1;
+    }
