9cf0e42f11d283a133285dbfabed68b6beaff6a1
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-06-13 12:02:35 +0530

    Create Rotate Doubly linked list by N nodes.cpp

---
+++ LoveBabber/Rotate Doubly linked list by N nodes.cpp
@@ -0,0 +1,19 @@
+Node *solve(Node*head,int k){
+    if(k==0)return head;
+    Node*temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    //For making circular
+    head->prev=temp;
+    temp->next=head;
+    while(k--){
+        temp=temp->next;
+    }
+
+    Node*nex=temp->next;
+    nex->prev=NULL;
+    temp->next=NULL;
+    return nex;
+}
+© 2022 GitHub, Inc.
