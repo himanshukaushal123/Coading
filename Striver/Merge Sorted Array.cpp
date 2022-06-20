5fd728812b7fdd10786b1f1e926204616bcbff67
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-06-20 12:16:00 +0530

    Update Merge Sorted Array.cpp

--- Striver/Merge Sorted Array.cpp
+++ Striver/Merge Sorted Array.cpp
@@ -5,3 +5,13 @@
         }
         return;
     }
+//2nd
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+      int i=m,j=0;
+        while(i<nums1.size()){
+            nums1[i]=nums2[j];
+            j++;i++;
+        }
+        sort(nums1.begin(),nums1.end());
+        return;
+    }
